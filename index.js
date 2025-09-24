2a6fedcd690e219b7ea76918280a87ae1d0d25c0
Alokdev07 <alokbhuyan162@gmail.com>
2025-09-24 13:50:40 +0530

    list is covered in this part

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ async function get_data(){
     console.log(`result -> ${result}`)
 } // this function is to get the data from the redis server
 
-get_data() // result -> sunil which is correct data
+// get_data() // result -> sunil which is correct data
 
 async function set_data(){
     await client.set('user:4','hey from node_server')
@@ -26,7 +26,28 @@ async function set_data(){
     console.log(`result -> ${result}`)
 }
 
-set_data()
+// set_data()
+
+async function list() {
+    await client.lpush('messages', 1)
+    await client.lpush('messages', 2)
+    await client.lpush('messages', 3)
+    await client.lpush('messages', 4)
+
+    // get length properly
+    const length = await client.llen('messages') // this is how we get the data length in a list
+
+    for (let i = 0; i < length; i++) {
+        let first_result = await client.rpop('messages')
+        let second_result = await client.blpop('messages',5)
+        console.log(first_result,second_result)
+    } // this is how we get the data in stack or queue fashion
+
+    let third_result = await client.lrange("messages", 1, 3)
+    console.log(third_result) // this is how we get the range in between in a list
+}
+
+// list()
 
 app.listen(PORT,() => {
     console.log(`listening on port ${PORT}`)
